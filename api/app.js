89ac45cca020f1709b5abbee037d6957c12d41d2
kennzuki <kingsyken@gmail.com>
2024-07-29 18:16:47 +0300

    routes with auth user and post route

--- api/app.js
+++ api/app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import postRoute from "./routes/post.route.js"
 
 const app = express();
 console.log('test');
+
+app.use("/api/posts",postRoute)
+
 app.listen(5000, () => {
-    console.log('server running');
-})
+  console.log('server running');
+});
